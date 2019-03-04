c4d7906471a18ed16c7f01d7a40b8c0a44b918bd
Marco Cameriero <me@marcocameriero.net>
2019-03-04 15:26:50 +0100

    Make RTC functions const.

--- include/avr-utils/drivers/DS1307_rtc.hpp
+++ include/avr-utils/drivers/DS1307_rtc.hpp
@@ -14,13 +14,13 @@ public:
     }
 
     /** Fills the given DateTime with the current time read from the RTC source. */
-    DateTime now();
+    DateTime now() const;
 
     /** Updates the current RTC with the given DateTime structure. */
-    void adjustNow(const DateTime&);
+    void adjustNow(const DateTime&) const;
 
     /** Returns a value indicating wether the RTC is enabled or not. */
-    bool isRunning();
+    bool isRunning() const;
 
 private:
     const uint8_t _address;
--- src/drivers/DS1307_rtc.cpp
+++ src/drivers/DS1307_rtc.cpp
@@ -9,7 +9,7 @@ static uint8_t bin2bcd(uint8_t x) { return x + 6 * (x / 10); }
 
 namespace avr {
 
-DateTime RTC::now() {
+DateTime RTC::now() const {
     // According to the DS1307 datasheet, first we need to do a write to indicate
     // the first address we want to read from the device (which is 0 in out case
     // since we want to read everything), then we can request all the time info.
@@ -31,7 +31,7 @@ DateTime RTC::now() {
     return dt;
 }
 
-void RTC::adjustNow(const DateTime& dt) {
+void RTC::adjustNow(const DateTime& dt) const {
     I2C::start(_address, i2c::I2CDirection::Write);
     I2C::write(0); // Start the write from the register 0 onwards
     I2C::write(bin2bcd(dt.seconds));
@@ -44,7 +44,7 @@ void RTC::adjustNow(const DateTime& dt) {
     I2C::stop();
 }
 
-bool RTC::isRunning() {
+bool RTC::isRunning() const {
     I2C::start(_address, i2c::I2CDirection::Write);
     I2C::write(0);
 
