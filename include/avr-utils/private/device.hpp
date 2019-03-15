324fe7c0169ccaa005571bac8cbf83d950aa0a1a
Marco Cameriero <me@marcocameriero.net>
2019-03-15 23:26:09 +0100

    Include also old macro names in device recognition.

--- include/avr-utils/private/device.hpp
+++ include/avr-utils/private/device.hpp
@@ -42,9 +42,9 @@ AVR_UTILS_SPECIALIZE_PORT_TRAITS(F)
 // Timer mappings are dependent of the current microcontroller,
 // so include the file with the appropriate definitions.
 
-#if defined(__AVR_ATmega48A__) || defined(__AVR_ATmega48PA__) || \
-    defined(__AVR_ATmega88A__) || defined(__AVR_ATmega88PA__) || \
-    defined(__AVR_ATmega168A__) || defined(__AVR_ATmega168PA__) || \
+#if defined(__AVR_ATmega48__) || defined(__AVR_ATmega48P__) || defined(__AVR_ATmega48A__) || defined(__AVR_ATmega48PA__) || \
+    defined(__AVR_ATmega88__) || defined(__AVR_ATmega88P__) || defined(__AVR_ATmega88A__) || defined(__AVR_ATmega88PA__) || \
+    defined(__AVR_ATmega168__) || defined(__AVR_ATmega168P__) || defined(__AVR_ATmega168A__) || defined(__AVR_ATmega168PA__) || \
     defined(__AVR_ATmega328__) || defined (__AVR_ATmega328P__)
 #   include "avr-utils/private/device/atmegaxx8.hpp"
 #else
